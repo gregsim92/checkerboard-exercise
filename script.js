14fdb8858be9d458d48d71e51267725bb5ea5f10
Greg Simpson <Greg@Gregs-MacBook-Air.local>
2015-11-04 13:08:00 -0800

    added a basic checkerboard pattern

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+	body.style.margin = "0px";
+
+var i = 0;
+
+while (i<63) {
+	var square = document.createElement("div");
+
+		square.style.margin = "0px";
+		square.style.width = "11.11%";
+		square.style.paddingBottom = "11.11%";
+		square.style.float = "left";
+		square.style.display = "inline-block";
+
+		if (i % 2 === 0){
+			square.style.backgroundColor = "black";
+		} else {
+			square.style.backgroundColor = "red";
+		}
+	document.body.appendChild(square);
+	i++;
+
+};
+
