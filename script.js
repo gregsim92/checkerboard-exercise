c2af42ccecc46f3e8e1cca93113b4ce8be998da2
Greg Simpson <Greg@Gregs-MacBook-Air.local>
2015-11-05 10:27:51 -0800

    added a transparency gradient to the basic checkerboard

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ var body = document.getElementsByTagName('body')[0];
 	body.style.margin = "0px";
 
 var i = 0;
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-
-    console.log(color);
-    return color;
+function isNextRow (squareNum){
+	if (squareNum % 9 === 0){
+		return true;
+	}
+	return false;
 }
+
+var alpha = 1;
 
 while (i<63) {
 	var square = document.createElement("div");
@@ -22,7 +19,17 @@ while (i<63) {
 		square.style.paddingBottom = "11.11%";
 		square.style.float = "left";
 		square.style.display = "inline-block";
-		square.style.background = getRandomColor();
+
+		if (isNextRow(i)){
+			alpha = alpha - 0.1;
+		}
+
+		if (i % 2 === 0){
+			square.style.backgroundColor = "rgba(255,0,0," + atlpha +")";
+		} else {
+			square.style.backgroundColor = "rgba(0,0,0," + alpha +")";
+		};
+
 	document.body.appendChild(square);
 	i++;
 
